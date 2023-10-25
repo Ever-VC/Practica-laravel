327ab37fba884b0bd60f87896a3c9aefff38c827
Ever VC <ever.vasquezc27@gmail.com>
2023-10-25 08:14:34 -0600

    :feat: Controlador y modelo creados

---
+++ app/Http/Controllers/ProyectoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Proyecto;
+use Illuminate\Http\Request;
+
+class ProyectoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Proyecto $proyecto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Proyecto $proyecto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Proyecto $proyecto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Proyecto $proyecto)
+    {
+        //
+    }
+}
